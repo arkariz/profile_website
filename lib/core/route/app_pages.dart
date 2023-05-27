b240ad847fe14f9811f239d75c74fd1d8e4d9b1d
arkariz <muhammadrisky1401@gmail.com>
2023-05-27 18:56:43 +0700

    feat: add login route

---
+++ lib/core/route/app_pages.dart
@@ -0,0 +1,15 @@
+import 'package:get/route_manager.dart';
+import 'package:mapala_unjani/core/route/app_routes.dart';
+import 'package:mapala_unjani/features/login/presentation/screens/login_screen.dart';
+
+class AppPages {
+  AppPages._();
+
+  static final routes = [
+    GetPage(
+      name: Routes.LOGIN,
+      page: () => const LoginScreen(),
+      // binding: SplashScreenBinding(),
+    ),
+  ];
+}
